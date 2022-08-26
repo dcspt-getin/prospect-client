c7c2ccc5dfa72549ee7326e86fe80b2b3e4e2ce0
Nuno Silva <nuno4slb@gmail.com>
2022-08-26 09:54:33 +0100

    deploy to ua server

--- deploy-ua.sh
+++ deploy-ua.sh
@@ -1,6 +1,6 @@
 export REACT_APP_ENV=production
-export REACT_APP_API_BASE_URL=https://dcspt-drivitup.ua.pt/protc/api
-export PUBLIC_URL=/protc/app/
+export REACT_APP_API_BASE_URL=https://dcspt-drivitup.ua.pt/housepref/api
+export PUBLIC_URL=/housepref/app/
 
 yarn build
-scp -r ./build/* nmsilva90@dcspt-drivitup.ua.pt:/var/www/drivitup-web/pythonapp/protc_client
+scp -r ./build/* nmsilva90@dcspt-drivitup.ua.pt:/var/www/drivitup-web/pythonapp/housepref_client
